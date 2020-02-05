9c636c8c5fc755ac4709bcbc3788a88a84fc35a5
Ali Caglayan <alizter@gmail.com>
2020-02-05 14:26:15 +0100

    possible to specify directory in configure script

--- configure.sh
+++ configure.sh
@@ -2,16 +2,18 @@
 
 rm -f Makefile.coq Makefile.HoTT
 
-if [ "$1" == "HoTT" ]
+#   This is a modification of the original configuration file to only
+#   build the HoTT files and to be able to configure the directory
+#   in which the HoTT library can be found.
+
+dir1=${1:-$PWD}
+
+if command -v $dir1/hoqc >/dev/null 2>&1
 then
-    if command -v hoqc >/dev/null 2>&1
-    then coq_makefile -f _HoTTProject -o Makefile.HoTT
-    else echo "Error: hoqc not found in path"
-    fi
+    sed -i "8s@.*@COQC = \"$dir1/hoqc\"@" _HoTTProject
+    sed -i "9s@.*@COQTOP = \"$dir1/hoqtop\"@" _HoTTProject
+    sed -i "10s@.*@COQDEP = \"$dir1/hoqdep\"@" _HoTTProject
+    coq_makefile -f _HoTTProject -o Makefile.HoTT
 else
-    echo "Building Coq version (default)"
-    if command -v coqtop >/dev/null 2>&1
-    then coq_makefile -f _CoqProject -o Makefile.coq
-    else echo "Error: coqtop not found in path"
-    fi
+    echo "Error: hoqc not found in $dir1"
 fi
